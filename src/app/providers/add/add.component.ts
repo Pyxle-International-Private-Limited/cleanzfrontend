6842859472ef0b4eacffe0109218fd476e5f3a6d
vidura <vidura.hemawansa@pyxle.io>
2020-06-09 17:40:08 +0530

    fix validation issues

--- src/app/providers/add/add.component.ts
+++ src/app/providers/add/add.component.ts
@@ -33,7 +33,7 @@ export class AddComponent implements OnInit {
   ngOnInit(): void {
     this.validateForm = this.fb.group({
       name: ["", [Validators.pattern('^[a-zA-Z ]*$'), Validators.required]],
-      address: ["",[Validators.email, Validators.required]],
+      address: ["",[Validators.required]],
       email: ["",[Validators.email, Validators.required]],
       phone: ["",[Validators.required]],
       isActive: []
